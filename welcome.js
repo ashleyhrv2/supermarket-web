8a19a9c82faf9a1a6c99796dc42c3470112d012c
ashleyhrv2 <168749264+ashleyhrv2@users.noreply.github.com>
2025-04-10 10:23:32 -0400

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,13 @@
+window.addEventListener("load",addListener);
+
+function addListener()
+{
+	document.getElementById("btnsubmit").addEventListener("click",play);
+	
+}
+
+function play()
+{
+	alert("hi");
+	
+}
